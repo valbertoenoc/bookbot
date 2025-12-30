95b61fbf0aa82ddb8f62ae10727ec9e0dddc087c
valbertoenoc <valbertoenoc@gmail.com>
2025-12-30 13:48:21 -0300

    add script to download test data (books)

---
+++ download_books.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget -O books/frankenstein.txt https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/frankenstein.txt
+wget -O books/mobydick.txt https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/mobydick.txt
+wget -O books/prideandprejudice.txt https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/prideandprejudice.txt
